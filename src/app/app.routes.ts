187f6e74aa4a5f63537b8ae73c151ceeaeabbcb4
saif-elhady <saifelhady2004@gmail.com>
2024-09-09 10:29:45 +0300

    lazyLoading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { authGuard } from './guards/auth.guard';
-import { ProductsComponent } from './products/products.component';
-import { ProductDetailsComponent } from './productDetails/product-details.component';
-import { ForgetPasswordComponent } from './forgetPassword/forget-password.component';
-import { ReviewsComponent } from './reviews/reviews.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', canActivate: [authGuard], component: HomeComponent },
-  { path: 'products', canActivate: [authGuard], component: ProductsComponent },
-  { path: 'products/:id', canActivate: [authGuard], component: ProductDetailsComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'products', component: ProductsComponent },
-  { path: 'products/:id', component: ProductDetailsComponent },
-  { path: 'myReviews', canActivate: [authGuard], component: ReviewsComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'forgetPassword', component: ForgetPasswordComponent },
+  { path: 'products', loadComponent: () => import('./products/products.component').then(m => m.ProductsComponent) },
+  { path: 'products/:id', loadComponent: () => import('./productDetails/product-details.component').then(m => m.ProductDetailsComponent) },
+  { path: 'myReviews', canActivate: [authGuard], loadComponent: () => import('./reviews/reviews.component').then(m => m.ReviewsComponent) },
+  { path: 'signup', loadComponent: () => import('./signup/signup.component').then(m => m.SignupComponent) },
+  { path: 'login', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent) },
+  { path: 'forgetPassword', loadComponent: () => import('./forgetPassword/forget-password.component').then(m => m.ForgetPasswordComponent) },
   { path: '**', component: NotFoundComponent }
 ];
